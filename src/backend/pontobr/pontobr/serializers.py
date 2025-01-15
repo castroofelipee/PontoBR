ef6f106615e8731264127d610e2b8fe8e5db46b3
castroofelipee <castroofelipee19@gmail.com>
2025-01-15 19:02:58 -0300

    wip

--- src/backend/pontobr/pontobr/serializers.py
+++ src/backend/pontobr/pontobr/serializers.py
@@ -4,5 +4,22 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
+    confirm_password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "username",
+            "passoword",
+            "confirm_password",
+        ]
+
+        def validate(self, data):
+            if data['password'] != data['confirm_password']:
+                raise serializers.ValidationError({'password': 'Password do not match'})
