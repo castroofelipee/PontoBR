fdb5a569b492e37161d0c85c12fb5ec0309ec34a
castroofelipee <castroofelipee19@gmail.com>
2025-01-15 19:06:42 -0300

    wip

--- src/backend/pontobr/pontobr/serializers.py
+++ src/backend/pontobr/pontobr/serializers.py
@@ -21,5 +21,17 @@ class RegisterSerializer(serializers.ModelSerializer):
         ]
 
         def validate(self, data):
-            if data['password'] != data['confirm_password']:
-                raise serializers.ValidationError({'password': 'Password do not match'})
+            if data["password"] != data["confirm_password"]:
+                raise serializers.ValidationError({"password": "Password do not match"})
+            return data
+
+        def create(self, validated_data):
+            validated_data.pop("confirm_password")  # Remove confirm_password
+            user = User.objects.create_superuser(
+                username=validated_data["username"],
+                email=validated_data["email"],
+                first_name=validated_data["first_name"],
+                last_name=validated_data["last_name"],
+                password=validated_data["password"],
+            )
+            return user
