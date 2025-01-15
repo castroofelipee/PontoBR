00c8e886e7a1f2589a53ed9943bd0afef395da26
castroofelipee <castroofelipee19@gmail.com>
2025-01-15 18:56:06 -0300

    wip

--- src/backend/pontobr/pontobr/models.py
+++ src/backend/pontobr/pontobr/models.py
@@ -2,4 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class 
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    
