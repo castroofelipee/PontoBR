5cc2c1314a0b000e394879216a317c1ec3d253d0
castroofelipee <castroofelipee19@gmail.com>
2025-01-15 18:59:10 -0300

    wip

--- src/backend/pontobr/pontobr/serializers.py
+++ src/backend/pontobr/pontobr/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
